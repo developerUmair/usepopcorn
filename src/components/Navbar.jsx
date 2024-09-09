e75c90c49cb1ec0c566d2fb881991ed4d41322e1
developerUmair <umairmak143@gmail.com>
2024-09-09 16:17:34 +0500

    making separate component for each block of code by breaking app.js

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import Search from "./Search";
+import Logo from "./Logo";
+import Numresults from "./Numresults";
+
+const Navbar = ({}) => {
+  return (
+    <nav className="nav-bar">
+      <Logo />
+      <Search />
+      <Numresults />
+    </nav>
+  );
+};
+
+export default Navbar;

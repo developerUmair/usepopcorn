fbe8024f5a5426f010f6569eea3316a170030d4f
developerUmair <umairmak143@gmail.com>
2024-10-22 16:20:22 +0500

    add focus on input using ref and clearing input by click enter key

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const Search = ({query, setQuery}) => {
+const Search = ({ query, setQuery }) => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputRef.current) {
+        return;
+      }
+
+      if (e.code === "Enter") {
+        inputRef.current.focus();
+        setQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [setQuery]);
 
   return (
     <input
+      ref={inputRef}
       className="search"
       type="text"
       placeholder="Search movies..."
